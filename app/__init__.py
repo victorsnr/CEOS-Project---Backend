0db9337deaa34b9315ba106a449e11e4bb8dc8a4
victorsnr <irsamuelrodriguesv@gmail.com>
2026-05-25 20:10:03 -0300

    ajustes de configuração

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from routes.web import web
-from routes.api import api
-from extensions import db
+from routes.users import api_users
+from routes.tasks import api_tasks
+from routes.auth import api_auth
+from extensions import db, jwt
 
 def create_app():
 
@@ -11,11 +12,14 @@ def create_app():
     app.config.from_object("config.Config")
 
     db.init_app(app)
-    
+    jwt.init_app(app)
+
     with app.app_context():
         db.create_all()
 
-    app.register_blueprint(api, url_prefix = '/api')
+    app.register_blueprint(api_tasks, url_prefix = '/api')
+    app.register_blueprint(api_users, url_prefix = '/api')
+    app.register_blueprint(api_auth, url_prefix = '/api')
     app.register_blueprint(web)
 
     return app
--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
